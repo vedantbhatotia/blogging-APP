10b10b056bd0f73cbe2885d74974d6ac74800def
vedantbhatotia <vedantbhatotia@gmail.com>
2024-03-28 00:19:46 +0530

    adding backend routes

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,93 @@
+import { Hono, Next } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate';
+import{decode,jwt,sign,verify} from 'hono/jwt'
+export const BlogRouter = new Hono<{
+    Bindings:{
+      DATABASE_URL:string
+    },
+    Variables:{
+        userId:string
+    }
+  }>();
+const secret = "magic"
+const authMiddleware = async (c: any,next:Next) => {
+    try {
+        const token: string = c.req.header("Authorization").split(" ")[1];
+        if (token !== null || token !== undefined) {
+          const decode = await verify(token, secret);
+          if (decode) {
+            c.set('userId', decode);
+            await next();
+          } else {
+            return c.body("you are unauthroized user sorry", 401);
+          }
+        } else {
+          return c.body("you are unauthroized user", 401);
+        }
+      } catch (error) {
+        return c.body("unauthroized ", 401);
+      }
+};
+BlogRouter.post('/', authMiddleware,async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+	const userId = c.get('userId');
+	const body = await c.req.json();
+	const post = await prisma.post.create({
+		data: {
+			title: body.title,
+			content: body.content,
+			authorId: Number(userId)
+		}
+	});
+	return c.json({
+		id: post.id
+	});
+});
+
+BlogRouter.put('/',authMiddleware, async (c) => {
+	const userId = Number(c.get('userId'));
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	prisma.post.update({
+		where: {
+			id: body.id,
+			authorId: userId
+		},
+		data: {
+			title: body.title,
+			content: body.content
+		}
+	});
+
+	return c.text('updated post');
+});
+BlogRouter.get('/bulk',async (c)=>{
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+    const blogs = await prisma.post.findMany();
+    return c.json(
+        blogs
+    )
+})
+BlogRouter.get('/:id',authMiddleware, async (c) => {
+	const id = Number(c.req.param('id'));
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+	
+	const post = await prisma.post.findUnique({
+		where: {
+			id:id
+		}
+	});
+
+	return c.json(post);
+})
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,73 @@
+import { Hono, Next } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate';
+import{decode,jwt,sign,verify} from 'hono/jwt'
+export const userRouter = new Hono<{
+    Bindings:{
+      DATABASE_URL:string
+    }
+  }>();
+const secret = "magic"
+const authMiddleware = async (c:any, next:Next) => {
+    const token:string = c.req.headers['authorization'];
+    if (!token) {
+      return c.json({ error: 'Unauthorized' }).status(401);
+    }
+    try {
+      const decodedToken = await verify(token, secret);
+      if(!decodedToken){
+        return c.text("unauthorized request");
+      }else{
+        c.set("userId",decodedToken.id)
+        await next();
+      }
+    } catch (error) {
+      return c.json({ error: 'Invalid token' }).status(401);
+    }
+  };
+  userRouter.post('/signup',async(c)=>{
+    const prisma = new PrismaClient({
+      datasourceUrl:c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+    const body = await c.req.json();
+    const existing_user = await prisma.user.findUnique({
+      where:{
+        email:body.email,
+        name:body.name
+      }
+    })
+    if(existing_user){
+      return c.text("user already exists sign in ")
+    }else{
+      const user = await prisma.user.create({
+        data:{
+          email:body.email,
+          name:body.name,
+          password:body.password
+        }
+      })
+      const token = await sign(user.id,secret);
+      return c.json(token);
+    }
+  })
+  
+  
+  
+  userRouter.post('/signin',async (c)=>{
+    const prisma = new PrismaClient({
+      datasourceUrl:c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+    const body = await c.req.json();
+    const existing_user = await prisma.user.findUnique({
+      where:{
+        email:body.email,
+        name:body.name
+      }
+    })
+    if(existing_user){
+      const token = await sign(existing_user.id,secret);
+      return c.json(token);
+    }else{
+      return c.text("user not found sign up ")
+    }
+  })
