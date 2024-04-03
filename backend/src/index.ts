2fbddd96c99c5a4a29efc4b13a6d37cbecba277f
vedantbhatotia <vedantbhatotia@gmail.com>
2024-04-03 09:17:37 +0530

    connecting backend to frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,13 @@
 import { Hono, Next } from 'hono'
 import { userRouter } from './routes/user';
 import { BlogRouter } from './routes/blog';
+import { cors } from 'hono/cors';
 const app = new Hono<{
   Bindings:{
     DATABASE_URL:string
   }
 }>();
+app.use('*',cors())
 app.route('api/v1/user',userRouter)
 app.route('api/v1/blog',BlogRouter)
 export default app
--- frontend/src/components/auth.tsx
+++ frontend/src/components/auth.tsx
@@ -1,12 +1,13 @@
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+const backend_url = "https://backend.vedantbhatotia.workers.dev"
 interface AuthProps {
     type: string;
 }
-
 function Auth({ type }: AuthProps) {
-    const [username, setUsername] = useState<string>("");
+    const navigate = useNavigate();
+    const [email, setEmail] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
@@ -14,14 +15,28 @@ function Auth({ type }: AuthProps) {
         setName(e.target.value);
     };
 
-    const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
     };
 
     const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
         setPassword(e.target.value);
     };
-
+    async function SendRequest(){
+        try{
+            const response = await axios.post(`${backend_url}/api/v1/user/${type === "signup"?"signup" : "signin"}`,{
+                    name:name,
+                    email:email,
+                    password:password
+            });
+            const jwt = response.data
+            localStorage.setItem("token",jwt)
+            navigate('/blogs');
+        }
+        catch(err){
+            console.log("error making the request");
+        }
+    }
     return (
         <div className="h-screen flex justify-center flex-col">
             <div className="flex justify-center">
@@ -38,10 +53,10 @@ function Auth({ type }: AuthProps) {
                         </div>
                     </div>
                     <div className="pt-8">
-                        <LabelledInput label="Name" placeholder="Vedant Bhatotia..." onChange={handleNameChange} />
-                        <LabelledInput label="Username" placeholder="vedant@gmail.com" onChange={handleUsernameChange} />
+                        <LabelledInput label="Name" type = "text" placeholder="Vedant Bhatotia..." onChange={handleNameChange} />
+                        <LabelledInput label="Email"  type = "text"placeholder="vedant@gmail.com" onChange={handleEmailChange} />
                         <LabelledInput label="Password" type="password" placeholder="123456" onChange={handlePasswordChange} />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                        <button onClick={SendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                     </div>
                 </div>
             </div>
@@ -53,14 +68,14 @@ interface LabelledInputType {
     label: string;
     placeholder: string;
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    type?: string;
+    type: string;
 }
 
 function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
     return (
         <div>
             <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-            <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+            <input onChange={onChange} type={type} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
         </div>
     );
 }
